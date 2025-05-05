3276012fa38087c38285aad1c755bd703df0a61d
anthoni portocarrero rodriguez <softwaretoni21@gmail.com>
2025-05-05 16:00:54 -0500

    feat: Add ecommerce client actions and types for product details and listing

---
+++ src/lib/actions/ecommerce/ecommerce-client.action.ts
@@ -0,0 +1,36 @@
+import { httpClient } from "@/lib/api/http-client";
+import {
+  ProductClientFilters,
+  ProductDetailClientResponse,
+  ProductsClientResponse,
+} from "@/types/ecommerce/client/ecommerce.types";
+
+export async function getDetailProductClient(id: number) {
+  try {
+    return await httpClient<ProductDetailClientResponse>(
+      `/api/products/${id}/item/with-clients`,
+      {
+        method: "GET",
+      }
+    );
+  } catch (error) {
+    console.error("Error al obtener el producto:", error);
+    throw error;
+  }
+}
+
+export async function getProductsClient(
+  filters?: ProductClientFilters
+): Promise<ProductsClientResponse> {
+  try {
+    return await httpClient<ProductsClientResponse>(
+      "/api/products/list/with-clients?isActive=true",
+      {
+        params: filters as Record<string, unknown>,
+      }
+    );
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    throw error;
+  }
+}
---
+++ src/types/ecommerce/client/ecommerce.types.ts
@@ -0,0 +1,71 @@
+export interface ProductsClientResponse {
+  success: boolean;
+  items: Item[];
+  meta: Meta;
+}
+
+export interface Item {
+  id: number;
+  name: string;
+  description: string;
+  sku: string;
+  memberPrice: number;
+  publicPrice: number;
+  stock: number;
+  status: string;
+  isActive: boolean;
+  category: Category;
+  imagesCount: number;
+  createdAt: Date;
+  updatedAt: Date;
+  mainImage: string;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  code: string;
+}
+
+export interface Meta {
+  totalItems: number;
+  itemsPerPage: number;
+  totalPages: number;
+  currentPage: number;
+}
+
+export interface ProductDetailClientResponse {
+  success: boolean;
+  product: Product;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  sku: string;
+  memberPrice: number;
+  publicPrice: number;
+  stock: number;
+  status: string;
+  isActive: boolean;
+  category: Category;
+  imagesCount: number;
+  createdAt: Date;
+  updatedAt: Date;
+  images: Image[];
+}
+
+export interface Image {
+  id: number;
+  url: string;
+  isMain: boolean;
+  order: number;
+}
+
+export interface ProductClientFilters {
+  categoryId?: number;
+  name?: string;
+  page?: number;
+  limit?: number;
+}
